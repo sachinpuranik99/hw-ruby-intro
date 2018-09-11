59c508a04581439f963daaac4664ee8cc3ce5d41
EC2 Default User <ec2-user@ip-172-31-9-130.us-east-2.compute.internal>
2018-09-11 03:36:51 +0000

    First part coded and clean

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -9,21 +9,27 @@ end
 
 def max_2_sum arr
   # YOUR CODE HERE
+  # array.each { |x| puts x }
+  @numbers = arr.sort
+  sum @numbers.last(2)
 end
 
 def sum_to_n? arr, n
   # YOUR CODE HERE
+  @numbers = arr.combination(2).map { |x, y| x + y }
+  @numbers.include? n
 end
 
 # Part 2
 
 def hello(name)
   # YOUR CODE HERE
-  "Hello" + name
+  "Hello, " + name
 end
 
 def starts_with_consonant? s
   # YOUR CODE HERE
+  
 end
 
 def binary_multiple_of_4? s
